2693b2d61bb21ad3985b2c185ab9f5bd69d21574
mikolajm203 <mikolajeczus0@gmail.com>
2021-01-05 16:18:23 +0100

    Added check_win

--- Tile.js
+++ Tile.js
@@ -1,38 +1,61 @@
 class Tile{
-    static t_count = 0;
-    constructor(){
-      this.r = tileSize - 10;
-      this.c_y = 0;
-      this.cy_a = 0;
-      this.cy_v = 0;
-      this.c_x = 50;
-      this.is_laying = false;
-      if(Tile.t_count % 2 === 0){
-        this.color = "#007b83";
-      }
-      else{
-        this.color = "#ff847c";
-      }
-      Tile.t_count++;
+  static t_count = 0;
+  constructor(){
+    this.r = tileSize - 10;
+    this.c_y = 0;
+    this.cy_a = 0;
+    this.cy_v = 0;
+    this.c_x = 50;
+    this.is_laying = false;
+    if(Tile.t_count % 2 === 0){
+      this.color = "#007b83";
     }
-    update(selCol, columns){
-      if(this.c_y === 0){
-        this.c_x = selCol;
-      }
-      this.cy_v += this.cy_a;
-      this.c_y += this.cy_v;
-      if(this.c_y >= height - 50){
-        this.cy_a = 0;
-        this.cy_v = 0;
-        this.c_y = height - 50;// - tileSize;
-        this.is_laying = true;
-        columns[selCol]++;
-      }
-      return columns;
+    else{
+      this.color = "#ff847c";
     }
-    show(){
-      stroke(this.color);
-      fill(this.color);
-      circle(this.c_x, this.c_y, this.r);
+    Tile.t_count++;
+  }
+  update(selCol, columns, index){
+    if(this.c_y === 0){
+      this.c_x = selCol * tileSize + 50;
+    }
+    this.cy_v += this.cy_a;
+    this.c_y += this.cy_v;
+    if(this.c_y >= height - 50 - 100 * columns[index]){
+      this.land(columns, index);
+    }
+    win_state = check_win();
+    return columns;
+  }
+  show(){
+    stroke(this.color);
+    fill(this.color);
+    circle(this.c_x, this.c_y, this.r);
+  }
+  insert(index){
+    for(var i = 0; i < board[index].length; i++){
+      if(i === 6){
+        if(this.color === "#007b83")
+          board[index][i] = 1;    // blue - 1
+        else
+          board[index][i] = 2;    // red - 2
+        return i;
+      }
+      if(board[index][i + 1] !== 0){
+        if(this.color === "#007b83")
+          board[index][i] = 1;    // blue - 1
+        else
+          board[index][i] = 2;    // red - 2
+        return i;
+      }
     }
   }
+  land(columns, index){
+    this.cy_a = 0;
+    this.cy_v = 0;
+    this.c_y = height - 50 - 100 * columns[index];
+    this.is_laying = true;
+    columns[index]++;
+    this.insert(index);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -2,36 +2,111 @@ const tileSize = 100;
 var canv;
 var selCol;
 var tiles = [];
-var columns = [0, 0, 0, 0, 0];
+var columns = [0, 0, 0, 0, 0, 0, 0];
+var index;
+var board = [[0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0]];
+var win_state = 0;
+function check_win(){
+  var length = 1;
+  var prev = 0;
+  // vertical lines
+  for(var i = 0; i < board.length; i++){
+    for(var j = 0; j < board.length; j++){
+      if(board[i][j] !== 0 && board[i][j] === prev){
+        length++;
+      }
+      else{
+        prev = board[i][j];
+        length = 1;
+      }
+      if(length >= 4)
+        return prev;
+    }
+  }
+  // horizontal lines
+  length = 1;
+  prev = 0;
+  for(var i = 0; i < board.length; i++){
+    for(var j = 0; j < board.length; j++){
+      for(var j = 0; j < board.length; j++){
+        if(board[j][i] !== 0 && board[j][i] === prev){
+          length++;
+        }
+        else{
+          prev = board[j][i];
+          length = 1;
+        }
+        if(length >= 4)
+          return prev;
+      }
+    }
+  }
+  // diagonal (top left - bottom right) lines
+  length = 1;
+  prev = 0;
+  const noOfDiagonals = 7;
+  for(var i = 0; i < 7; i++){
+
+    if(length >= 4)
+      return prev;
+  }
+  // diagonal(bottomleft - top right) lines
+  length = 1;
+  prev = 0;
+  for(var i= 0 ; i < 7; i++){
+    if(length >= 4){
+      return prev;
+    }
+  }
+  return 0;
+}
 function mousePressed(){
   tiles[Tile.t_count - 1].is_laying = false;
   tiles[Tile.t_count - 1].c_y = 0;
   tiles[Tile.t_count - 1].cy_a = 1;
+  index = selCol;
 }
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
+  var y = (windowHeight - height) / 2 + 50;
   canv.position(x, y);
 }
 function setup() {
-  canv = createCanvas(500, 600);
+  canv = createCanvas(700, 700);
   centerCanvas();
   tiles.push(new Tile());
+  console.log(board);
 }
 function draw() {
-  background(42, 54, 59);
-  for(var i = 0; i <= 5; i++){
-    strokeWeight(1);
-    stroke("#fecea8");
-    line(tileSize * i, 0, tileSize * i, height);
+  if(win_state === 0){
+    background(42, 54, 59);
+    for(var i = 0; i <= 7; i++){
+      strokeWeight(1);
+      stroke("#fecea8");
+      line(tileSize * i, 0, tileSize * i, height);
+    }
+    selCol = Math.floor(mouseX/tileSize);
+    columns = tiles[Tile.t_count - 1].update(selCol, columns, index);
+    tiles.forEach(element => {
+      element.show();
+    });
+    if(tiles[Tile.t_count - 1].is_laying){
+      tiles.push(new Tile());
+    }
   }
-  selCol = Math.floor(mouseX/tileSize) * tileSize + 50;
-  columns = tiles[Tile.t_count - 1].update(selCol, columns);
-  for(var i = 0; i < Tile.t_count; i++){
-    tiles[i].show();
-  }
-  if(tiles[Tile.t_count - 1].is_laying){
-    tiles.push(new Tile());
+  else{
+    background(42, 54, 59);
+    noLoop();
+    if(win_state === 1)
+      console.log("blue won :)");
+    else if(win_state === 2)
+      console.log("red won :)");
   }
 }
 
